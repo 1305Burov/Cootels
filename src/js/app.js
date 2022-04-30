c488a6c4c399ad422c21ce94b5e3f405e30f5885
1305Burov <1305burov@gmail.com>
2022-04-30 18:24:02 +0300

    swiper added

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,17 @@ flsFunc.isWebp();
 accordion();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  });
